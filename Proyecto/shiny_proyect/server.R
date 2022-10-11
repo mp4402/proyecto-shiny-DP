a170864f39afa9c62c1ba729ea773df76cb52a40
CarlosAlvarado-git <61555440+CarlosAlvarado-git@users.noreply.github.com>
2022-10-10 18:42:04 -0600

    listo

--- Proyecto/shiny_proyect/server.R
+++ Proyecto/shiny_proyect/server.R
@@ -2,14 +2,18 @@ library(shiny)
 library(readr)
 library(dplyr)
 library(DT)
+library(readxl)
 
 shinyServer(function(input, output) {
   
   tabla_rating <- read_csv('ratings.csv')
+  tabla_movies <- readxl::read_excel('movies.xlsm')
+  tabla_genome_scores <- read_csv("genome-scores.csv")
+  tabla_genome_tags <- read_csv("genome-tags.csv")
+  
   
   output$out_slider_input_multi<- renderTable({
     #browser()
-    a <- as.integer(input$slinputmulti)
     tabla_rating %>%
           select(movieId, rating) %>%
           group_by(movieId) %>%
