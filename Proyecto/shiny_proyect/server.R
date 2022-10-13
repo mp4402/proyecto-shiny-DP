72aea497aee526a0a2af9556b5335134c8374a90
CarlosAlvarado-git <61555440+CarlosAlvarado-git@users.noreply.github.com>
2022-10-13 17:41:00 -0600

    cambios

--- Proyecto/shiny_proyect/server.R
+++ Proyecto/shiny_proyect/server.R
@@ -3,13 +3,11 @@ library(readr)
 library(dplyr)
 library(DT)
 library(readxl)
-
+tabla_rating <- read_csv('ratings.csv')
+tabla_movies <- readxl::read_excel('movies.xlsm')
+tabla_genome_scores <- read_csv("genome-scores.csv")
+tabla_genome_tags <- read_csv("genome-tags.csv")
 shinyServer(function(input, output) {
-  
-  tabla_rating <- read_csv('ratings.csv')
-  tabla_movies <- readxl::read_excel('movies.xlsm')
-  tabla_genome_scores <- read_csv("genome-scores.csv")
-  tabla_genome_tags <- read_csv("genome-tags.csv")
   
   
   output$out_slider_input_multi<- renderTable({
@@ -17,7 +15,22 @@ shinyServer(function(input, output) {
     tabla_rating %>%
           select(movieId, rating) %>%
           group_by(movieId) %>%
-          summarize(r = mean(rating)) %>%
-          filter(r >=  input$slinputmulti[1] & r <= input$slinputmulti[2])
+          summarize(rating = mean(rating)) %>%
+          filter(rating >=  input$slinputmulti[1] & rating <= input$slinputmulti[2])%>%
+          left_join(tabla_movies, by = c("movieId" = "movieId")) %>%
+          arrange(desc(rating))
+  })
+  
+  output$out_select_input_multi<- renderTable({
+    #browser()
+    #tabla_genome_tags junto tabla_genome_scores para unir por tag y movieId
+    tabla_genome_tags %>%
+      filter(tabla_genome_tags$tag %in% input$select_tags) %>%
+      left_join(tabla_genome_scores, by = c("tagId" = "tagId")) %>%
+      left_join(tabla_movies, by = c("movieId" = "movieId")) %>%
+      select(title,relevance, tag) %>%
+      filter(relevance >= 0.30) %>%
+      arrange(desc(relevance))
+    
   })
 })
--- Proyecto/shiny_proyect/ui.R
+++ Proyecto/shiny_proyect/ui.R
@@ -1,26 +1,49 @@
-
 library(shiny)
+library(readr)
+library(dplyr)
+library(DT)
+library(readxl)
+tabla_genome_tags <- read_csv("genome-tags.csv")
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("UI dinamico, UpdateInput"),
+  titlePanel("Analisis de películas"),
   
   tabsetPanel(
-    tabPanel('Rango de ratings movies',
+    tabPanel('Rango de ratings',
              sliderInput('slinputmulti','Seleccione Rango:',
                          value = c(0,5), 
                          min = 0, 
                          max = 5, 
-                         step = 0.5)
+                         step = 0.5),
+             mainPanel(
+               h3("Peliculas"),
+               tableOutput('out_slider_input_multi')
+               
+             )
     ),
-    mainPanel(
-      h1("Outputs"),
-      h3('Slider Input Multiple'),
-      tableOutput('out_slider_input_multi')
+    tabPanel('Tags',
+             selectInput('select_tags', 'Seleccione categorias de peliculas',
+                         choices = tabla_genome_tags$tag, selected = '007', multiple = TRUE),
+             mainPanel(
+               h3("Peliculas y su relevancia segun las tags"),
+               tableOutput('out_select_input_multi')
+               
+             )
+    ),
+    tabPanel('Usuario',
+             sidebarLayout(
+               sidebarPanel(
+                 
+               ),
+               sidebarPanel()
+             )
       
     )
+    
+    
   )
   
 ))
